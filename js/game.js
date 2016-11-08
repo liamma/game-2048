9e49af2cedbb468adac672e5453f105f88c57e84
liamma <majinghe2015@gmail.com>
2016-11-08 11:33:36 +0800

    random num

--- js/game.js
+++ js/game.js
@@ -19,15 +19,17 @@ Game2048.prototype = {
             this.getSelectGrid.append(tile);
             this.tiles[i] = tile;
         }
+        this.randomTile();
+        this.randomTile();
     },
-    newTile: function(val) {
-        /*
+    /*
         create one tile
         1.class tile is base css sheet
         2.class tile+num is num css sheet
         3.attr index is tile sort
         4.attr val is num
         */
+    newTile: function(val) {
         var tile = document.createElement("div");
         this.setTileVal(tile, val);
         return tile;
@@ -36,7 +38,20 @@ Game2048.prototype = {
         tile.className = 'tile tile' + val;
         tile.setAttribute('val', val);
         tile.innerHTML = val > 0 ? val : '';
-
+    },
+    /*
+    create a random num
+    create num is 2 or 4
+    */
+    randomTile: function() {
+        var eligibilityTiles = [];
+        // Traversal and find eligibility tile
+        for (var i = 0; i < this.tiles.length; i++) {
+            if (this.tiles[i].getAttribute('val') == 0)
+                eligibilityTiles.push(this.tiles[i]);
+        }
+        var randomTile = eligibilityTiles[Math.floor(Math.random() * eligibilityTiles.length)];
+        this.setTileVal(randomTile, Math.random() < 0.5 ? 2 : 4);
     }
 }
 var game;
